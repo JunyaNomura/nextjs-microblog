d5523352eac2aea61de83a7793572f3f43058a19
野村淳也 <unijack@unijackMacMini.local>
2023-02-01 00:31:07 +0900

    lesson progressed

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,52 @@ import { Inter } from '@next/font/google'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
 import Product from '@/components/Product'
+import { getPostsData } from '../lib/post';
 
-export default function Home() {
+//SSGの場合（外部から1度だけデータをとってくる）
+export async function getStaticProps() {
+  const allPostsData = getPostsData(); //id, title, date, thumbnail
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+//SSRの場合
+// export async function getServerSideProps(context) {
+
+//   return {
+//     props: {
+//       //コンポーネントに渡すためのprops
+//     }
+//   }
+// }
+
+export default function Home({ allPostsData }) {
+  console.log(allPostsData);
   return (
     <>
       <Layout>
         <section>
-          <p class="text-lg mt-4 max-w-2xl mx-auto">テクノロジー系のビジネスマンです。英語、フランス語、中国語を勉強しています。好きなプログラミング言語はGoです。</p>
+          <p className="text-lg mt-4 max-w-2xl mx-auto">テクノロジー系のビジネスマンです。英語、フランス語、中国語を勉強しています。好きなプログラミング言語はGoです。</p>
         </section>
 
         <section className="mt-8">
           <h2 className="text-2xl font-semibold">旅人のブログ</h2>
-          <div>
-            <article class="">
-              <Link href="#">
-                <img src="/images/img3.jpg" class="h-96 w-96 object-cover hover:translate-x-2 hover:translate-y-2 duration-200 border-r-12 border-b-8 border-r-indigo-500 border-b-indigo-500 hover:border-none" />
-              </Link>
-              <Link href="#">
-                  自由が丘散策
-              </Link>
-              <br />
-              <small>February 23, 2022</small>
-            </article>
+          <div className="grid grid-cols-2 gap-2">
+            {allPostsData.map((post) => (
+              <article key={post.id} className="">
+                <Link href={`posts/${post.id}`}>
+                  <img src={post.thumbnail} className="h-96 w-96 object-cover duration-200" />
+                </Link>
+                <Link href={`posts/${post.id}`}>
+                  {post.title}
+                </Link>
+                <br />
+                <small>{post.date}</small>
+              </article>
+            ))}
           </div>
 
         </section>
